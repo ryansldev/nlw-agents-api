7f647b6c4bd377aba3671eddb1405ab6e92ea6b7
ryansldev <ryanlima1309@gmail.com>
2025-07-12 10:22:14 -0300

    fix seed

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -2,12 +2,12 @@ import { reset, seed } from 'drizzle-seed';
 import { db, sql } from './connection.ts';
 import { schema } from './schema/index.ts';
 
-await reset(db, { schema });
+await reset(db, schema);
 
 await seed(db, schema).refine((f) => {
   return {
     rooms: {
-      count: 20,
+      count: 5,
       columns: {
         name: f.companyName(),
         description: f.loremIpsum(),
